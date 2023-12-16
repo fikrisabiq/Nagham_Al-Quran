f35c9b0ec9ce7081c3cfb3847eaf8073cf5366cf
jumli-gazali <ahmadjumli77@gmail.com>
2023-12-16 16:50:21 +0700

    add ignore file and finished vip feature

--- capstone-md/app/src/main/java/com/capstone/naghamalquran/ui/vip/VipDetail.kt
+++ capstone-md/app/src/main/java/com/capstone/naghamalquran/ui/vip/VipDetail.kt
@@ -65,7 +65,7 @@ class VipDetail : AppCompatActivity() {
 
 // Menambahkan pendengar OnCompletionListener untuk memulai ulang audio saat selesai
         mediaPlayer?.setOnCompletionListener {
-            hearMeButton.text = "Play" // Ganti dengan teks yang sesuai
+            hearMeButton.text = getString(R.string.btn_play) // Ganti dengan teks yang sesuai
             mediaPlayer?.seekTo(0) // Mengatur pemutaran kembali ke awal
         }
 
@@ -74,11 +74,11 @@ class VipDetail : AppCompatActivity() {
             // Memeriksa apakah MediaPlayer sedang berjalan
             if (mediaPlayer?.isPlaying == true) {
                 mediaPlayer?.pause()
-                hearMeButton.text = "Dengarkan" // Ganti dengan teks yang sesuai
+                hearMeButton.text = getString(R.string.btn_resume) // Ganti dengan teks yang sesuai
             } else {
                 // Jika tidak sedang berjalan, mulai memutar
                 mediaPlayer?.start()
-                hearMeButton.text = "Pause" // Ganti dengan teks yang sesuai
+                hearMeButton.text = getString(R.string.btn_pause) // Ganti dengan teks yang sesuai
             }
         }
     }
@@ -98,15 +98,46 @@ class VipDetail : AppCompatActivity() {
 
     private fun getAudioResource(keyNagham: String): Int {
         return when (keyNagham) {
-            "Full Syair" -> R.raw.tausyih_bayati
-            "Bayati Ashli Qorror" -> R.raw.tausyih_bayati
-//            "Bayati Ashli Nawa" -> R.raw.nama_file_bayati_ashli_nawa
-//            "Bayati Syuri" -> R.raw.nama_file_bayati_syuri
-//            "Bayati Suri Jawabuljawab" -> R.raw.nama_file_bayati_suri_jawabuljawab
-//            "Bayati Husaini" -> R.raw.nama_file_bayati_husaini
+            "Ar-Rahman Bayati (1-13)" -> R.raw.ar_rahman_bayati
+            "Ar-Rahman Bayati Ashli Qorror" -> R.raw.ar_rahman_bayati_ashli_qorror
+            "Ar-Rahman Bayati Ashli Nawa" -> R.raw.ar_rahman_bayati_ashli_nawa
+            "Ar-Rahman Bayati Syuri" -> R.raw.ar_rahman_bayati_syuri
+            "Ar-Rahman Bayati Husaini" -> R.raw.ar_rahman_bayati_husaini
+            "Ar-Rahman Bayati Ashli Jawab" -> R.raw.ar_rahman_bayati_ashli_jawab
+            "Ar-Rahman Bayati Syuri Jawabuljawab" -> R.raw.ar_rahman_bayati_syuri_jawabuljawab
+
+            "Ar-Rahman Shaba (14-15)" -> R.raw.ar_rahman_shaba
+            "Ar-Rahman Shaba Ashli" -> R.raw.ar_rahman_shaba_ashli
+            "Ar-Rahman Jawab Shaba" -> R.raw.ar_rahman_jawab_shaba
+            "Ar-Rahman Jawab Shaba Maalajam" -> R.raw.ar_rahman_jawab_shaba_maalajam
+            "Ar-Rahman Jawab Shaba Maalbastanjar" -> R.raw.ar_rahman_jawab_shaba_maalbastanjar
+
+            "Ar-Rahman Hijaz (16-18)" -> R.raw.ar_rahman_hijaz
+            "Ar-Rahman Hijaz Ashli" -> R.raw.ar_rahman_hijaz_ashli
+            "Ar-Rahman Hijaz Kar" -> R.raw.ar_rahman_hijaz_kar
+            "Ar-Rahman Hijaz Karkur" -> R.raw.ar_rahman_hijaz_karkur
+
+            "Ar-Rahman Rast (19-23)" -> R.raw.ar_rahman_rast
+            "Ar-Rahman Rast Zunjiran" -> R.raw.ar_rahman_rast_zunjiran
+            "Ar-Rahman Rast Alanawa" -> R.raw.ar_rahman_rast_alanawa
+
+            "Ar-Rahman Sika (24-27)" -> R.raw.ar_rahman_sika
+            "Ar-Rahman Sika Ashli" -> R.raw.ar_rahman_sika_ashli
+            "Ar-Rahman Sika Mishri" -> R.raw.ar_rahman_sika_mishri
+            "Ar-Rahman Sika Turki" -> R.raw.ar_rahman_sika_turki
+
+            "Ar-Rahman Jiharkah (28-30)" -> R.raw.ar_rahman_jiharkah
+            "Ar-Rahman Jiharkah Ashli" -> R.raw.ar_rahman_jiharkah_ashli
+            "Ar-Rahman Jiharkah Jawab" -> R.raw.ar_rahman_jiharkah_jawab
+
+            "Ar-Rahman Nahawand (31-33)" -> R.raw.ar_rahman_nahawand
+            "Ar-Rahman Nahawand Ashli" -> R.raw.ar_rahman_nahawand_ashli
+            "Ar-Rahman Nahawand Jawab" -> R.raw.ar_rahman_nahawand_jawab
+
             else -> R.raw.error // Ganti dengan file audio default atau resource lainnya
         }
     }
+
 
 
 }
